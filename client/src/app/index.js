f7d953e8d99087132e01df5a0ceb55219cea9aa7
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-04-22 17:06:37 -0500

    not showing create/update content

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import { NavBar } from '../components'
+import { ItemsList, ItemsInsert, ItemsUpdate } from '../pages'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -9,6 +10,15 @@ function App() {
     return (
         <Router>
             <NavBar />
+            <Switch>
+                <Route path="/items/list" exact component={ItemsList} />
+                <Route path="/items/create" exact component={ItemsInsert} />
+                <Route
+                    path="/items/update/:id"
+                    exact
+                    component={ItemsUpdate}
+                />
+            </Switch>
         </Router>
     )
 }
---
+++ client/src/pages/ItemsList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ItemsList extends Component {
+    render() {
+        return (
+            <div>
+                <p>In this page you'll see the list of items</p>
+            </div>
+        )
+    }
+}
+
+export default ItemsList
