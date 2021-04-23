9a52df7b2c96a94037d38b38b12d1b577efc718f
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-04-23 13:30:37 -0500

    fixed delete bug, hot realoading docker works too

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	cd server && $(MAKE) build
 	cd client && $(MAKE) build
 
-run:
+run-dev:
 	docker-compose up
 
 stop:
--- client/src/components/Links.jsx
+++ client/src/components/Links.jsx
@@ -19,7 +19,7 @@ class Links extends Component {
         return (
             <React.Fragment>
                 <Link to="/" className="navbar-brand">
-                    Cloth-Web
+                    Cloth Web
                 </Link>
                 <Collapse>
                     <List>
