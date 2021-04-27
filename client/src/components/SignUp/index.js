29d7f0fad51db60944bedfc9b049f07a0b7ea259
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-04-26 23:52:51 -0500

    firebase imort error

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,9 +1,88 @@
-import React from "react";
-
-const SignUp = () => (
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+ 
+import * as ROUTES from '../../constants/routes';
+ 
+const SignUpPage = () => (
   <div>
     <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
 
-export default SignUp;
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
+ 
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+ 
+  onSubmit = event => {
+ 
+  }
+ 
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+ 
+  render() {
+    const {
+      username,
+      email,
+      password,
+      error,
+    } = this.state;
+
+    const isInvalid =
+      password === '' ||
+      email === '' ||
+      username === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="password"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <button disabled={isInvalid} type="submit">Sign Up</button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+ 
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+ 
+export default SignUpPage;
+ 
+export { SignUpForm, SignUpLink };
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
-import './index.css';
+
 //import * as serviceWorker from './serviceWorker';
  
-import App from './components/App';
+import App from './app';
 import Firebase, { FirebaseContext } from './components/Firebase';
  
 ReactDOM.render(
