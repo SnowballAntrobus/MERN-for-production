e965ecf0ebfea02dafe44af1be207bb722f8f7c5
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-04-26 22:29:27 -0500

    firebase singleton instance

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+ 
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+ 
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById('root'),
+);
